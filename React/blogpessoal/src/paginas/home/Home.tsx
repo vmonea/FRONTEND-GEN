a2e15b8226c32852fa04ce195bd0d8390db8076d
vmonea <vitormonea2000@gmail.com>
2024-05-15 18:50:03 -0300

    add: react

---
+++ React/blogpessoal/src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/1137px-React.svg.png" alt="Imagem Tela Inicial" className="img"/>
+        </>
+    );
+}
+
+export default Home;
